e5196adecf49adb6347b27f4bfbecbde3a54c8c2
antoshkaxxr <63500718+antoshkaxxr@users.noreply.github.com>
2024-04-23 17:01:37 +0500

    Task 2

--- static/focus.js
+++ static/focus.js
@@ -25,24 +25,19 @@ run();
 
 function sendRequest(url) {
     return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.response));
-                } else {
-                    reject(new Error(`Request failed with status ${xhr.status}`));
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Request failed with status ${response.status}`);
                 }
-            }
-        };
-
-        xhr.onerror = function () {
-            reject(new Error("Request failed"));
-        };
-
-        xhr.send();
+                return response.json();
+            })
+            .then(data => {
+                resolve(data);
+            })
+            .catch(error => {
+                reject(new Error("Request failed"));
+            });
     });
 }
 
